1a82419825bf3823f2b2d217b8750f71b181faeb
Luke Pulverenti <luke.pulverenti@gmail.com>
2018-04-24 13:24:51 -0400

    initial

---
+++ IMVDb/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MediaBrowser.Common.Plugins;
+
+namespace IMVDb
+{
+    public class Plugin : BasePlugin
+    {
+        private Guid _id = new Guid("0277E613-3EC0-4360-A3DE-F8AF0AABB5E9");
+        public override Guid Id
+        {
+            get { return _id; }
+        }
+
+        public override string Name
+        {
+            get { return StaticName; }
+        }
+
+        public static string StaticName
+        {
+            get { return "IMVDb"; }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return "IMVDb metadata for musiv videos";
+            }
+        }
+    }
+}
